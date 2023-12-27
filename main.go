65c3e01d6fda6755ce93df2cbc63e4edc6c7bb25
TPride <tranielmp8@gmail.com>
2023-12-26 18:04:08 -0600

    deploying again

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/tranielmp8/go-api-project/db"
+	// "github.com/tranielmp8/go-api-project/db"
 	"github.com/tranielmp8/go-api-project/routes"
 )
 
 func main() {
-	db.InitDB()
+	// db.InitDB()
 
 	server := gin.Default()
 	routes.RegisterRoutes(server)
